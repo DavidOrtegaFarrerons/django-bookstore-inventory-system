58ce9c244ad50ddd65b7bc5644bd669a8ae1a41a
David Ortega Farrerons <davidortegafarrerons@gmail.com>
2024-12-05 21:40:04 +0100

    Add author, genre and book models. Add books to Installed Apps. Create migrations.

--- books/models.py
+++ books/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
